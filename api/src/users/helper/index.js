0af7fb5dfa340fb107d7d6a3c31ae23ae25463ad
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-04 00:07:41 +0100

    add helpers

---
+++ api/src/users/helper/index.js
@@ -0,0 +1,16 @@
+const HttpError = require("./HttpError");
+const BadRequestError = require("./BadRequestError");
+const handleMongooseError = require("./handleMongooseError");
+const signToken = require("./signToken");
+const { hashPassword, hashedPassword } = require("./hashPassword");
+const comparePassword = require("./comparePassword"); 
+
+module.exports = {
+  HttpError,
+  BadRequestError,
+  handleMongooseError,
+  signToken,
+  hashPassword,
+  hashedPassword,
+  comparePassword,
+};
