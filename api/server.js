5c01ddb86ba57e4fd562b17a06508d3b8ccd0347
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-04 00:01:59 +0100

    add structure user user

--- api/server.js
+++ api/server.js
@@ -3,18 +3,15 @@
 /* -------------------------------------------------------------------------- */
 
 const app= require('./src/app');
-require("dotenv").config();
-require('./src/config/database')()
-
-
-const {  PORT } = process.env;
+const {PORT} = process.env;
 const PORTS = PORT || 5000;
 
+require("dotenv").config();
+require('./src/config/database')()
 
 
 
 app.listen(PORTS,()=>{
 console.log(`Server is running on port ${PORT}`);
 });
-
 
