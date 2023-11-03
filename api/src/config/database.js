457847fcaa7b69733dd7dac2bfbe753763742bae
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-03 22:58:08 +0100

    new fill config database

---
+++ api/src/config/database.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+
+const { DB_CONNECT } = process.env;
+
+const connectToMongo = async () => {
+    await mongoose.connect(DB_CONNECT)
+    
+.then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+  
+  };
+
+module.exports = connectToMongo
+
+
