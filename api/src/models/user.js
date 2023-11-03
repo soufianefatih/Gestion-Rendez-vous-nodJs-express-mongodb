cfecdf36333ec562dbc99f70ae388b4708d08382
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-03 23:05:33 +0100

    add models user

---
+++ api/src/models/user.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      default: "User",
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    role: {
+      type: String
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+
+
+
+const User = model("user", userSchema);
+
+module.exports = User;
