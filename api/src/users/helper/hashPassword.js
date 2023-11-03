28e67aa3541d5e6e0e5ceef57b40a5a125325259
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-04 00:10:08 +0100

    add bycrypt

--- api/src/users/helper/hashPassword.js
+++ api/src/users/helper/hashPassword.js
@@ -1,20 +1,5 @@
 const bcrypt = require("bcrypt");
 
-// const hashPassword = async function (next) {
-//   const salt = await bcrypt.genSalt();
-//   const hash = bcrypt.hashSync(this.password, salt);
-//   this.password = hash;
-//   next();
-// };
-
-// const hashedPassword = async function (password) {
-//   const salt = await bcrypt.genSalt();
-//   const hashedPassword = bcrypt.hashSync(password, salt);
-//   return hashedPassword;
-// };
-
-// module.exports = { hashPassword, hashedPassword };
-
 const hashPassword = async function (next) {
   try {
     const salt = await bcrypt.genSalt();
--- api/src/users/helper/index.js
+++ api/src/users/helper/index.js
@@ -1,14 +1,8 @@
-const HttpError = require("./HttpError");
-const BadRequestError = require("./BadRequestError");
-const handleMongooseError = require("./handleMongooseError");
 const signToken = require("./signToken");
 const { hashPassword, hashedPassword } = require("./hashPassword");
 const comparePassword = require("./comparePassword"); 
 
 module.exports = {
-  HttpError,
-  BadRequestError,
-  handleMongooseError,
   signToken,
   hashPassword,
   hashedPassword,
