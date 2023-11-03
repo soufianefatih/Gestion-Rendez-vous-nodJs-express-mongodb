a679d1c7354da8d653aec847f064a594352acf0c
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-03 22:53:34 +0100

    new fill config

--- api/server.js
+++ api/server.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const app = express ();
+/* -------------------------------------------------------------------------- */
+/*                               index || Server                              */
+/* -------------------------------------------------------------------------- */
+
+const app= require('./src/app');
 require("dotenv").config();
 
 const {  PORT } = process.env;
---
+++ api/src/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+//* global midleware for not router
+app.use((req, res) => {
+  res.status(404).json({ message: "Root Not Found" });
+});
+
+module.exports = app;
