0675bd3d7d05d7dc3abf59b59fadedb66dd1788e
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-04 01:06:14 +0100

    add register

--- api/src/users/controller/authController.js
+++ api/src/users/controller/authController.js
@@ -4,7 +4,7 @@ const asyncHandler = require('express-async-handler')
 
 
 
-exports.register = asyncHandler(async (req, res) => {
+exports.registera = asyncHandler(async (req, res) => {
   try{ 
   const { value, error } =  registerSchema.validate(req.body, {
     abortEarly: false,
@@ -14,10 +14,6 @@ exports.register = asyncHandler(async (req, res) => {
 
   const { name, email, password} = value;
   
-  // Set the role based on the condition (data.type)
-     const data = req.body
-      const role = data.type ? 'admin' : 'user';
-
       if (error) {
         return res.status(400).json({ message: "Validation error", errors: error.details });      // throw  BadRequestError(error )
       }   
@@ -41,6 +37,37 @@ exports.register = asyncHandler(async (req, res) => {
   }
  
 });
+
+exports.register = asyncHandler(async (req, res) => {
+    try {
+      const { value, error } = registerSchema.validate(req.body, {
+        abortEarly: false,
+      });
+  
+      console.log('value', { value, error });
+  
+      if (error) {
+        return res.status(400).json({ message: "Validation error", errors: error.details });
+      }
+  
+      const { name, email, password } = value || {}; // Add this line to handle undefined value
+  
+      const result = await User.create({
+        name,
+        email,
+        password,
+      });
+  
+      res.status(201).json(result);
+    } catch (error) {
+      if (error.status === 409) {
+        return res.status(409).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Internal Server Error", error: error.message });
+      }
+    }
+  });
+  
 
 
 
