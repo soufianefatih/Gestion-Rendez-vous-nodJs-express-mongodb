05c2801693610bd0f657829fd556ca7bf71dd280
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-05 05:59:50 +0100

    add file errorMiddleware code

--- api/src/users/controller/authController.js
+++ api/src/users/controller/authController.js
@@ -1,11 +1,12 @@
 const { User } = require("../../models");
 const { registerSchema} = require("../schema");
 const asyncHandler = require('express-async-handler')
+const AppError = require('../../utils/HttpError');
 
 
 
-exports.registera = asyncHandler(async (req, res) => {
-  try{ 
+exports.registera = asyncHandler(async (req, res,next) => {
+  // try{ 
   const { value, error } =  registerSchema.validate(req.body, {
     abortEarly: false,
   });
@@ -15,7 +16,9 @@ exports.registera = asyncHandler(async (req, res) => {
   const { name, email, password} = value;
   
       if (error) {
-        return res.status(400).json({ message: "Validation error", errors: error.details });      // throw  BadRequestError(error )
+        const err = new AppError('Validation error', 404);
+        // return res.status(400).json({ message: "Validation error", errors: error.details });      // throw  BadRequestError(error )
+        return next(err)
       }   
   
   
@@ -27,14 +30,14 @@ exports.registera = asyncHandler(async (req, res) => {
   
       res.status(201).json(result);
 
-  }catch (error) {
-    if (error.status === 409) {
-      return res.status(409).json({ message: error.message});
+  // }catch (error) {
+  //   if (error.status === 409) {
+  //     return res.status(409).json({ message: error.message});
 
-    } else {
-      return res.status(500).json({ message: "Internal Server Error", error: error.message });
-    }
-  }
+  //   } else {
+  //     return res.status(500).json({ message: "Internal Server Error", error: error.message });
+  //   }
+  // }
  
 });
 
