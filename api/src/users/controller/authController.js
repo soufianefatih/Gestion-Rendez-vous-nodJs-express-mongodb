6660de8a2b90d9898547d1d5d09ef3b72bb5bbfa
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-05 06:22:40 +0100

    add file errorMiddleware code

--- api/src/users/controller/authController.js
+++ api/src/users/controller/authController.js
@@ -40,33 +40,28 @@ exports.registera = asyncHandler(async (req, res) => {
 });
 
 exports.register = async (req, res, next) => {
+  console.log('Request body:', req.body); // Add this line for debugging
+
   const { value, error } = registerSchema.validate(req.body, {
     abortEarly: false,
   });
 
+  console.log('Validation result:', { value, error }); // Add this line for debugging
+
   if (error) {
-    // Extract error details for better handling
-    const errorMessage = error.details.map((detail) => detail.message).join(', ');
-    const err = new AppError(`Validation error: ${errorMessage}`, 400);
+    const err = new AppError('Validation error', 400);
     return next(err);
   }
 
   const { name, email, password } = value || {};
 
-  try {
-    const result = await User.create({
-      name,
-      email,
-      password,
-    });
+  const result = await User.create({
+    name,
+    email,
+    password,
+  });
 
-    res.status(201).json(result);
-  } catch (err) {
-    // Handle other errors
-    console.error(err);
-    const internalError = new AppError('Internal Server Error', 500);
-    return next(internalError);
-  }
+  res.status(201).json(result);
 };
 
   
