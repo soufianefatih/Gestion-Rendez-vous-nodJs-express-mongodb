042fa1f87837e9b46323648d18b46ed8584d43e6
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-05 06:26:36 +0100

    add file errorMiddleware code

--- api/src/app.js
+++ api/src/app.js
@@ -4,7 +4,6 @@
 
 
 
-
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -12,19 +11,22 @@ const AppError = require('./utils/HttpError');
 const handleGlobalErrors = require('./utils/ErrorMiddleware');
 
 
-  //* Require Routes
-const authRoutes = require("./users/routes/userRoute");
-//* Register Our Routes
-app.use("/api/users", authRoutes);
-
  //* the will let us get data the data form post
  app.use(express.json());
  app.use(bodyParser.urlencoded({ extended: true }));
  app.use(bodyParser.json());
 
 
+
+//* Require Routes
+const authRoutes = require("./users/routes/userRoute");
+//* Register Our Routes
+app.use("/api/users", authRoutes);
+
+
+
 //* global midleware for not router
-app.all((req, res,err) => {
+app.use((req, res,err) => {
   res.status(404).json({ message: "Root Not Found" });
 });
 
@@ -44,7 +46,7 @@ app.get('/example', (req, res, next) => {
   next(err); // Pass the error to the next middleware
 });
 
- // Global error handling middleware (should be the last middleware)
+// Global error handling middleware (should be the last middleware)
 app.use(handleGlobalErrors);     
 
 
