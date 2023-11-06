55ce433c16c17b723d1edf2349005a28a6f1a9fe
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-06 04:01:43 +0100

    add new function

--- api/src/users/validator/checkUserExistence.js
+++ api/src/users/validator/checkUserExistence.js
@@ -1,1 +1,27 @@
-checkUserExistence
+const User = require('../../models'); // Replace with the correct path to your User model
+
+const checkUserExistence = async (req, res, next) => {
+  try {
+    const { id, email } = req.params; // Assuming you are checking by ID and email from request parameters
+
+    // Check if user with the given ID or email exists
+    const user = await User.findOne({ $or: [{ _id: id }, { email }] });
+
+    if (!user) {
+      // If user does not exist, return a 404 Not Found response
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Attach the user object to the request for later use in the route handlers
+    req.foundUser = user;
+
+    // Continue to the next middleware or route handler
+    next();
+  } catch (error) {
+    console.error(error);
+    // Handle other errors if needed
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+module.exports = checkUserExistence;
