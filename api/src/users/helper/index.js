a4f1b92d96e53d7619c1cdb92d58557b94ed9102
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-06 03:51:40 +0100

    handleMongooseError change

--- api/src/users/helper/index.js
+++ api/src/users/helper/index.js
@@ -1,10 +1,12 @@
 const signToken = require("./signToken");
 const { hashPassword, hashedPassword } = require("./hashPassword");
 const comparePassword = require("./comparePassword"); 
+const handleMongooseError = require("./handleMongooseError")
 
 module.exports = {
   signToken,
   hashPassword,
   hashedPassword,
   comparePassword,
+  handleMongooseError,
 };
