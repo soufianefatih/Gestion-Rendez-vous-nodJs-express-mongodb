6b7d96bf6078ff2f0e3c698df5c0d9345cf107cc
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-08 21:14:02 +0100

    add

---
+++ api/src/rendezvous/validator/checkRdvExistence.js
@@ -0,0 +1,25 @@
+const { Rdv } = require('../../models'); // Replace with the correct path to your User model
+const AppError = require('../../utils/HttpError');
+
+
+const checkRdvExistence = async (req, res, next) => {
+
+  try {
+    const existingUser = await Rdv.findOne();
+
+    if (!existingUser) {
+        const err = new AppError('rdv not exist', 409);
+       return next(err); // Pass the error to the next middleware
+    }
+
+    // If the user doesn't exist, proceed to the next middleware or route handler
+    next();
+  } catch (error) {
+    console.error("Error checking rdv:", error);
+    const err = new AppError('Internal Server Error', 500);
+     return next(err); // Pass the error to the next middleware
+
+  }
+};
+
+module.exports =checkRdvExistence;
