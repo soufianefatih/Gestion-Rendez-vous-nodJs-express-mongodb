aeb367c4c2315c5b7b6628ba76cf458366e7e716
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-23 16:38:45 +0100

    add

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import {
   Routes,
-  Route
+  Route,
+  BrowserRouter as Router
 } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,11 +14,13 @@ function App() {
   return (
     <div className="root">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+      <Router>
       <Routes>
-      <Route path="/login" element={checkIfNotLoginBeforeRenderComponent(<Login />)} />
+      <Route path="/" element={checkIfNotLoginBeforeRenderComponent(<Login />)} />
       <Route path="/register" element={checkIfNotLoginBeforeRenderComponent(<Register />)} />
 
       </Routes>
+      </Router>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </div>
     )
