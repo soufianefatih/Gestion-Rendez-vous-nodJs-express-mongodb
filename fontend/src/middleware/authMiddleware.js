b5dcca73dc77f5561e0e738abdff3307b96cebe3
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-23 17:30:19 +0100

    add

--- fontend/src/middleware/authMiddleware.js
+++ fontend/src/middleware/authMiddleware.js
@@ -6,7 +6,7 @@ export function isLogin() {
 }
 
 
-export function checkLoginBeforeRenderComponent(component, ifNotLoginRedirectToUrl = "/") {
+export function checkLoginBeforeRenderComponent(component, ifNotLoginRedirectToUrl = "/login") {
     let login = window.localStorage.getItem("login") ?? 0;
 
     if (login === 1) {
