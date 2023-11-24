6f6d2a17f4d5d128f1456577bcc799a3fdd5af9d
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-24 16:20:22 +0100

    new

--- fontend/src/index.js
+++ fontend/src/index.js
@@ -1,9 +1,9 @@
-// index.js
+
+import store from './redux/store/store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store/store';
 import App from './App';
 
 ReactDOM.render(
