2a1ef623434f20438e58f1d5ea0a894da2e0fc06
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-24 17:05:56 +0100

    ADD

--- fontend/src/redux/reducer/authReducer.js
+++ fontend/src/redux/reducer/authReducer.js
@@ -7,33 +7,23 @@ const authReducer = (state = initialState, action) => {
   console.log(action.type, action.payload); // Log action type and payload
   switch (action.type) {
     case 'LOGIN_SUCCESS':
-      const { user } = action.payload;
+      const { user, accessToken } = action.payload;
 
-      if (user && user.accessToken) {
-        // Check if accessToken is present in user object
-        const accessToken = user.accessToken || null;
-
-        if (accessToken) {
-          window.localStorage.setItem("token", accessToken);
-          window.localStorage.setItem("id", user.id);
-          window.localStorage.setItem("name", user.name);
-          window.localStorage.setItem("email", user.email);
-          window.localStorage.setItem("role", user.role);
-          window.localStorage.setItem("login", 1);
-        } else {
-          console.error('Error: Missing or invalid accessToken in user object', user);
-          // Handle the error, for example, redirect to an error page or display a message to the user
-          // You might dispatch another action to handle the error in a more structured way
-        }
+      if (user && accessToken) {
+        window.localStorage.setItem("token", accessToken);
+        window.localStorage.setItem("id", user.id);
+        window.localStorage.setItem("name", user.name);
+        window.localStorage.setItem("email", user.email);
+        window.localStorage.setItem("role", user.role);
+        window.localStorage.setItem("login", 1);
       } else {
-        console.error('Error: Missing user object in the response', action.payload);
+        console.error('Error: Missing or invalid accessToken in user object', user);
         // Handle the error, for example, redirect to an error page or display a message to the user
-        // You might dispatch another action to handle the error in a more structured way
       }
 
       return {
         ...state,
-        user: action.payload,
+        user: user,
       };
     case 'LOGOUT':
       // Clear localStorage on logout if needed
@@ -47,6 +37,8 @@ const authReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+
 
 
 
