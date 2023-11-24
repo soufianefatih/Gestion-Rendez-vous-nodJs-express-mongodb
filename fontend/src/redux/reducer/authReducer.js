001de2e6acb857a72dcd06022f8697e55bbccd1d
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-24 16:40:30 +0100

    add

--- fontend/src/redux/reducer/authReducer.js
+++ fontend/src/redux/reducer/authReducer.js
@@ -5,13 +5,26 @@ const initialState = {
   
 // authReducer.js
 const authReducer = (state = initialState, action) => {
+  console.log(action.type, action.payload); // Log action type and payload
   switch (action.type) {
     case 'LOGIN_SUCCESS':
+      // Update localStorage here
+      const { user } = action.payload;
+      window.localStorage.setItem("token", user.accessToken);
+      window.localStorage.setItem("id", user.id);
+      window.localStorage.setItem("name", user.name);
+      window.localStorage.setItem("email", user.email);
+      window.localStorage.setItem("role", user.role);
+      window.localStorage.setItem("login", 1);
+
       return {
         ...state,
         user: action.payload,
       };
     case 'LOGOUT':
+      // Clear localStorage on logout if needed
+      window.localStorage.clear();
+      
       return {
         ...state,
         user: null,
@@ -20,6 +33,7 @@ const authReducer = (state = initialState, action) => {
       return state;
   }
 };
+
 
   export default authReducer;
   
